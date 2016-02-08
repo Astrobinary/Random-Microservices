4916cb95f51af4f17cf6b666097f8a632f4b3e3f
Brandon <brandilla1337@gmail.com>
2016-02-08 16:01:41 -0500

    Added side menu.

--- public/js/main.js
+++ public/js/main.js
@@ -2,23 +2,60 @@
 
 $(document).ready(function () {
     var sidebarStatus = false;
-    $('.fa-bars').click(function () {
+    $('#icon').click(function () {
         if (sidebarStatus === false) {
+            sidebarStatus = true;
+
             $('.sidebar').animate({
                 width: '250px'
             }, 500);
             $('.card').animate({
                 marginLeft: '230px'
             }, 500);
-            sidebarStatus = true;
+
+            $('#icon').removeClass('fa-bars');
+            $('#icon').addClass('fa-arrow-left');
+
+            $('#icon').animate({
+                marginLeft: '200px'
+            }, 500);
+
+            $('.links').animate({
+                paddingLeft: '55px'
+            });
+
+            $('.blue').text('unix timestamp');
+            $('.green').text('file metadata');
+            $('.red').text('url shortener');
+            $('.purple').text('my address');
+            $('.orange').text('image abstraction');
+
         } else {
+            sidebarStatus = false;
+
             $('.sidebar').animate({
                 width: '50px'
             }, 500);
             $('.card').animate({
                 marginLeft: '30px'
             }, 500);
-            sidebarStatus = false;
+
+            $('#icon').removeClass('fa-arrow-left');
+            $('#icon').addClass('fa-bars');
+
+            $('#icon').animate({
+                marginLeft: '12px'
+            }, 500);
+            $('.links').animate({
+                paddingLeft: '0px'
+            });
+
+            $('.blue').text('unix');
+            $('.green').text('file');
+            $('.red').text('url');
+            $('.purple').text('add');
+            $('.orange').text('img');
+
         }
     });
 });
